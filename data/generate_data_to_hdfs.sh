89a8b07e92deef09813646ba5128129927651afb
Ioannis Protogeros <61976410+vanourogeros@users.noreply.github.com>
2024-01-11 00:39:39 +0200

    minor fix in generate_data_to_hdfs.sh

--- data/generate_data_to_hdfs.sh
+++ data/generate_data_to_hdfs.sh
@@ -5,6 +5,6 @@
 # and writes a csv file to the hdfs path specified (default:/data/generated_data.csv)
 # with hdfs dfs -put. Obviously you need to have and hdfs configured and set up
 
-$NUM_SAMPLES=${1:-5000}
-$HDFS_DATA_PATH=${2:-/data/generated_data.csv}
+NUM_SAMPLES=${1:-5000}
+HDFS_DATA_PATH=${2:-/data/generated_data.csv}
 python generate_data_stdout.py "$NUM_SAMPLES" | hdfs dfs -put - "$HDFS_DATA_PATH"
