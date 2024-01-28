39485d6b9dc09bccdcbdb2dae1bccce876810a1a
Chrysa Pratikaki <chrisaprat@gmail.com>
2024-01-28 19:09:29 +0200

    Update sort_ray.py

--- scripts/ETL/sort_ray.py
+++ scripts/ETL/sort_ray.py
@@ -3,14 +3,16 @@ from pyarrow import fs
 import sys
 
 
-
 ray.init()
+
+ctx = ray.data.DataContext.get_current()
+ctx.use_push_based_shuffle = True
+ctx.execution_options.resource_limits.cpu = int(sys.argv[1])
+
 hdfs_fs = fs.HadoopFileSystem.from_uri("hdfs://okeanos-master:54310")
-ds = ray.data.read_csv(sys.argv[1], filesystem=hdfs_fs) \
+
+ds = ray.data.read_csv([f"/data/large/dataset{i}.csv" for i in range(1,sys.argv[2])], filesystem=hdfs_fs) \
        .map_batches(lambda batch: batch)
 
-
-ds.sort("feature_1").show()
-
-# print(ds.count())
-print("stats:", ds.stats())
+ds_sorted = ds.sort("feature_1")
+ds_sorted.show()
